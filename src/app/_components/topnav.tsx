90119f378529431507f241776faebe804c1cbbe2
Denzell Mackay <denzell.mackay@hotmail.com>
2025-01-25 12:09:03 -0500

    updated auth. prod ready

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,18 +1,23 @@
-import { SignedIn, SignedOut, SignOutButton, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function TopNav() {
-    return (
-        <nav className="flex w-full items-center">
-            <div> Thing </div>
-            <div>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                    <SignOutButton />
-                </SignedIn>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-2xl font-semibold">
+      <div> PhotoThing </div>
+      <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,14 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { ClerkProvider, SignInButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import TopNav from "./_components/topnav";
 
 export const metadata: Metadata = {
   title: "ThingApp To be created",
   description: "Created to test a nice stack",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-
-function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-2xl font-semibold">
-      <div> PhotoThing </div>
-
-      <SignInButton />
-    </nav>
-  )
-}
 
 export default function RootLayout({
   children,
