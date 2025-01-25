7c924d408b12cb95c2afb4424b8aed8dc70c8a1e
Denzell Mackay <denzell.mackay@hotmail.com>
2025-01-25 10:27:33 -0500

    Updated to tailwind v4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "ThingApp To be created",
@@ -10,13 +10,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between">
+      <div> Navigation </div>
+    </nav>
+  )
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
-          <body className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">{children}</body>
+      <html lang="en">
+        <body className={`${GeistSans.variable}`}>
+          <TopNav />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
